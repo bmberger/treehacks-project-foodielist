f4a23115c303ebeac785df75d4b0627b6bee37e6
Noah Geller <geller.noah.s@gmail.com>
2020-02-15 13:54:43 -0800

    basic api functionality

--- recipes.py
+++ recipes.py
@@ -1,5 +1,16 @@
 import requests
+import json
+import sys
 
-r = requests.get("https://api.spoonacular.com/recipes/search?apiKey=2152c92e2c6749e182553d24f111ef70")
-print(r.status_code)
+# Query is supplied as an argument.
+queryString = " ".join(sys.argv[1:])
 
+# Perform GET request searching for the query.
+payload = {'apiKey' : '2152c92e2c6749e182553d24f111ef70', 'query' : queryString}
+r = requests.get("https://api.spoonacular.com/recipes/search", params=payload)
+
+info = json.loads(r.text)
+
+# Print out the titles of the matching recipes.
+for result in info['results']:
+    print(result['title'])
