4203b609b7b47c86d332eb5e07dea05c6ec22bc1
Qwyst <geller.noah.s@gmail.com>
2020-02-15 15:33:28 -0800

    added recipe summary functionality

--- recipes.py
+++ recipes.py
@@ -2,15 +2,26 @@ import requests
 import json
 import sys
 
+# Returns an HTML string summarizing the recipe with the given id.
+def summarizeRecipe(idNum, apiKey):
+    payload = {'apiKey' : apiKey}
+    r = requests.get("https://api.spoonacular.com/recipes/{}/summary".format(idNum), params=payload)
+    return json.loads(r.text)['summary']
+
 # Query is supplied as an argument.
 queryString = " ".join(sys.argv[1:])
 
+apiKey = '2152c92e2c6749e182553d24f111ef70'
+
 # Perform GET request searching for the query.
-payload = {'apiKey' : '2152c92e2c6749e182553d24f111ef70', 'query' : queryString}
+payload = {'apiKey' : apiKey, 'query' : queryString, 'number' : 3}
 r = requests.get("https://api.spoonacular.com/recipes/search", params=payload)
 
-info = json.loads(r.text)
+results = json.loads(r.text)
+numResults = results['totalResults']
 
-# Print out the titles of the matching recipes.
-for result in info['results']:
-    print(result['title'])
+# Print out the titles of the first 3 matching recipes.
+for result in results['results']:
+    print(result['title'] + ": " + str(result['id']))
+    print("Summary: {}".format(summarizeRecipe(result['id'], apiKey)))
+    print("")
