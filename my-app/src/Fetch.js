b8e98961e51a7a85444e5ae6dbfe2cecf28a22cf
Hannah P-W <hannahpw@stanford.edu>
2020-02-15 22:35:48 -0800

    fetch function

---
+++ my-app/src/Fetch.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const API = 'localhost:5000/api?query=';
+
+class Fetch extends React.Component {
+	constructor(props) {
+		super(props);
+	this.state = {
+		hits : [],
+	};
+	}
+	
+	render() {
+	{
+	    const { hits } = this.state;
+
+	    return (
+	      <ul>
+        	{hits.map(hit =>
+       		   <li key={hit.objectID}>
+        	    <a href={hit.url}>{hit.title}</a>
+       		   </li>
+       		 )}
+     		 </ul>
+  	  );
+	  }
+
+	}
+
+	componentDidMount() {
+		fetch(API + 'cheese')
+		.then(response => response.json())
+		.then(data => this.setState({ hits: data.hits }));
+	}
+	}
+export default Fetch;
